69c3d6f9091585a581cd0c8c47051bb7abef25a2
StrayDrop <straydrop411@gmail.com>
2019-07-03 20:18:07 +0900

    💖First Deploy

---
+++ README.md
@@ -0,0 +1,11 @@
+# Misato
+
+![u571](https://media.giphy.com/media/odsZocmsLGZH2/giphy.gif)
+
+# Mio-chan
+
+![u571](https://media.giphy.com/media/m0WvQNz58KSac/giphy.gif)
+
+# Sakamoto
+
+![u571](https://media.giphy.com/media/zL6Z2POEvbr8c/giphy.gif)
---
+++ precache-manifest.af15e1d8b21f9b9dfe9176a4d8563652.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebaab3f06508600ecd6a402bc2862d86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df0587b51c365dfccbf2",
+    "url": "/static/css/2.ee25fea5.chunk.css"
+  },
+  {
+    "revision": "51777890125e04b01923",
+    "url": "/static/css/main.9bb0c46e.chunk.css"
+  },
+  {
+    "revision": "df0587b51c365dfccbf2",
+    "url": "/static/js/2.440744f3.chunk.js"
+  },
+  {
+    "revision": "51777890125e04b01923",
+    "url": "/static/js/main.8c60f769.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e40fa723da26aaa06cb168ad5f0074a0",
+    "url": "/static/media/Bald Eagle-SoundBible.com-1234817251.e40fa723.mp3"
+  },
+  {
+    "revision": "8a485894462d09e1798a303e5bd87330",
+    "url": "/static/media/CocogooseClassic-ExtraBold.8a485894.ttf"
+  },
+  {
+    "revision": "319d6196ea4fb1048759d7d3a1e28ce0",
+    "url": "/static/media/Dolphins-SoundBible.com-1774583018.319d6196.mp3"
+  },
+  {
+    "revision": "448413d864c83021c1235c67e95209f5",
+    "url": "/static/media/MADEEvolveSansEVO-Light.448413d8.ttf"
+  },
+  {
+    "revision": "8eef57df5de27867f2604b506d1d0f0d",
+    "url": "/static/media/MADEEvolveSansEVO-Medium.8eef57df.ttf"
+  },
+  {
+    "revision": "f4c7112e44239ff1fcf0aebcc3871a6b",
+    "url": "/static/media/Orca-SoundBible.com-841491912.f4c7112e.mp3"
+  },
+  {
+    "revision": "48f624cfefcffa203f4aa7ca8c75fb3e",
+    "url": "/static/media/Red Shouldered Hawk-SoundBible.com-1383454521.48f624cf.mp3"
+  },
+  {
+    "revision": "90c980d2d9dd36a4bb0a6ffd2b0f8bd3",
+    "url": "/static/media/anchor-solid.90c980d2.svg"
+  },
+  {
+    "revision": "6e694e1c166a3415a67dc558f2936cd5",
+    "url": "/static/media/fish-solid.6e694e1c.svg"
+  },
+  {
+    "revision": "72af106d8d78fa8c212f59f98c9b78be",
+    "url": "/static/media/ship-solid.72af106d.svg"
+  },
+  {
+    "revision": "c0c094205d870b17693375379575a499",
+    "url": "/static/media/shower-solid.c0c09420.svg"
+  },
+  {
+    "revision": "a0b58f5c9db8f798b56959feb98c1a60",
+    "url": "/static/media/swimming-pool-solid.a0b58f5c.svg"
+  },
+  {
+    "revision": "ab90635dee9909b4002cdfba390ac410",
+    "url": "/static/media/water-solid.ab90635d.svg"
+  },
+  {
+    "revision": "f41b4737db98c6f438a3734e338f1e61",
+    "url": "/static/media/water.f41b4737.svg"
+  }
+]);
